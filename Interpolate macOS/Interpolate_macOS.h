3043c74cd8172d9f443bfbe94eb8f3b3095b2e81
Fabrice Armisen <farmisen@gmail.com>
2019-07-07 22:05:04 -0700

    Adding macOS target

---
+++ Interpolate macOS/Interpolate_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Interpolate_macOS.h
+//  Interpolate macOS
+//
+//  Created by FABRICE ARMISEN on 7/7/19.
+//  Copyright © 2019 Roy Marmelstein. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Interpolate_macOS.
+FOUNDATION_EXPORT double Interpolate_macOSVersionNumber;
+
+//! Project version string for Interpolate_macOS.
+FOUNDATION_EXPORT const unsigned char Interpolate_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Interpolate_macOS/PublicHeader.h>
+
+
